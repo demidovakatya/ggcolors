516598a3c370b05a66313539d80f3d0efb1b067f
Katya Demidova <demidovakatya@gmail.com>
2016-01-12 04:32:08 +0300

    fixed greys a little

--- ggcolors.R
+++ ggcolors.R
@@ -1,16 +1,28 @@
+require(gg)
 
+color.list <- colors()
+grey.list <- color.list[152:253]
+color.list <- color.list[-c(152:253, 260:361)]
 
-clrs <- colors()
-is.gr <- c(152:253, 260:361)
+gg.gr <- data.frame(x=rep(1:6, 17), y=rep(1:17, each=6),c=grey.list, stringsAsFactors = F)
 
-gg.gr <- data.frame(x=rep(1:12, 17), y=rep(1:17, each=12),c=clrs[is.gr], stringsAsFactors = F)
-gg.colors <- data.frame(x=rep(1:10, 45), y=rep(1:45, each=10), c=clrs[-c(is.gr,655:657)], stringsAsFactors = F)
+gg.colors <- data.frame(x=rep(1:10, 45), y=rep(1:45, each=10), c=color.list[-c(451:453)], stringsAsFactors = F)
 
-ggplot(gg.colors, aes(x=x, y=y, fill=c)) + geom_tile() + 
-  scale_fill_identity() + scale_colour_identity() + scale_x_continuous(name="", breaks=NULL, expand=c(0, 0)) + scale_y_continuous(name="", breaks=NULL, expand=c(0, 0)) + geom_text(aes(label=c), colour="black", size=2) 
-ggsave("colors.png")
+ggplot(gg.colors, aes(x=x, y=y, fill=c)) + 
+        geom_tile() + 
+        scale_fill_identity() + 
+        scale_colour_identity() + 
+        scale_x_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
+        scale_y_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
+        geom_text(aes(label=c), colour="black", size=2) 
+# ggsave("colors.png")
 
-ggplot(gg.gr, aes(x=x, y=y, fill=c)) + geom_tile() + 
-  scale_fill_identity() + scale_colour_identity() + scale_x_continuous(name="", breaks=NULL, expand=c(0, 0)) + scale_y_continuous(name="", breaks=NULL, expand=c(0, 0)) + geom_text(aes(label=c), colour="steelblue1", size=3) 
+ggplot(gg.gr, aes(x=x, y=y, fill=c)) + 
+        geom_tile() + 
+        scale_fill_identity() + 
+        scale_colour_identity() + 
+        scale_x_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
+        scale_y_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
+        geom_text(aes(label=c), colour="steelblue1", size=3) 
 ggsave("greys.png")
 
