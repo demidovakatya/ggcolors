5c80684244e32caada7afa52db79dc8c148c0c07
Katya Demidova <demidovakatya@gmail.com>
2016-01-12 04:40:17 +0300

    now colors is 5 * 90 chart

--- ggcolors.R
+++ ggcolors.R
@@ -6,7 +6,7 @@ color.list <- color.list[-c(152:253, 260:361)]
 
 gg.gr <- data.frame(x=rep(1:6, 17), y=rep(1:17, each=6),c=grey.list, stringsAsFactors = F)
 
-gg.colors <- data.frame(x=rep(1:10, 45), y=rep(1:45, each=10), c=color.list[-c(451:453)], stringsAsFactors = F)
+gg.colors <- data.frame(x=rep(1:5, 90), y=rep(1:90, each=5), c=color.list[-c(451:453)], stringsAsFactors = F)
 
 ggplot(gg.colors, aes(x=x, y=y, fill=c)) + 
         geom_tile() + 
@@ -15,7 +15,7 @@ ggplot(gg.colors, aes(x=x, y=y, fill=c)) +
         scale_x_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
         scale_y_continuous(name="", breaks=NULL, expand=c(0, 0)) + 
         geom_text(aes(label=c), colour="black", size=2) 
-# ggsave("colors.png")
+ggsave("colors.png", height = 14)
 
 ggplot(gg.gr, aes(x=x, y=y, fill=c)) + 
         geom_tile() + 
